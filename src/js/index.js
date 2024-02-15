f6bc024882a093790f953404df9533da60e7a08d
Kirill Pekarskiy <kipe122006@gmail.com>
2024-02-15 19:53:47 +0300

    Создание секции с услугами

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-
 const swiper = new Swiper('.swiper', {
     speed: 600,
     spaceBetween: 0,
@@ -33,3 +32,56 @@ const swiper = new Swiper('.swiper', {
     }
 }
 });
+
+let serviceBodies = document.querySelectorAll(".service-block__body");
+serviceBodies.forEach(serviceBody => {
+  let btnPrice = serviceBody.querySelector(".service-block__button");
+  let closeBtn = serviceBody.querySelector(".service-block__icon");
+
+  btnPrice.addEventListener("click", function() {
+    serviceBody.classList.add("service-block__body_act");
+  })
+  closeBtn.addEventListener("click", function() {
+    console.log(closeBtn)
+    serviceBody.classList.remove("service-block__body_act");
+  })
+})
+
+document.querySelectorAll(".dropdown-select").forEach(function(dropdownWrapper) {
+
+  const dropdownButton = dropdownWrapper.querySelector(".dropdown-select__button")
+  const dropdownGrid = dropdownWrapper.querySelector(".dropdown-select__grid")
+  const dropdownList = dropdownWrapper.querySelector(".dropdown-select__list")
+  const dropdownListItems =  dropdownWrapper.querySelectorAll(".dropdown-select__item")
+  const dropdownInput = dropdownWrapper.querySelector(".dropdown-select__input")
+  
+  dropdownButton.addEventListener("click", function() {
+     
+      dropdownGrid.classList.toggle("dropdown-select__grid_visible")
+      dropdownList.classList.toggle("dropdown-select__list_visible")
+      this.classList.add("dropdown-select__button_active");
+  })
+  
+  dropdownListItems.forEach(item => {
+      item.addEventListener("click", function (e) {
+          e.stopPropagation()
+
+          dropdownButton.textContent = this.textContent;
+          dropdownInput.value = this.dataset.value
+          dropdownButton.focus();
+          closeSelect()
+      })
+  })
+  
+  document.addEventListener("click", function(e) {
+      if (e.target != dropdownButton) {
+          dropdownButton.classList.remove("dropdown-select__button_active")
+          closeSelect()
+      }
+  })
+
+  function closeSelect() {
+    dropdownGrid.classList.remove("dropdown-select__grid_visible")
+    dropdownList.classList.remove("dropdown-select__list_visible")
+  }
+})
