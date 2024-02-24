857923b264450d7570f11c71663f3188a6788f62
Kirill Pekarskiy <kipe122006@gmail.com>
2024-02-24 23:01:16 +0300

    Создание функционала скрытия и появления скролла

--- src/js/modules/open-pop-up.js
+++ src/js/modules/open-pop-up.js
@@ -5,21 +5,54 @@ function popUpToggle() {
     let btnCloseMainForm = mainPopUpForm.querySelector(".main-form__close");
 
     let over = document.querySelector(".over");
+    let body = document.querySelector("body");
+    const timeout = 500;
+    let lockPadding = document.querySelectorAll(".lock-padding")
 
     buttonMains.forEach(buttonMain => {
         buttonMain.addEventListener("click", function() {
             mainPopUpForm.classList.add("pop-up_open");
             over.classList.add("over_open");
+            bodyLock();
         })
     })
     btnCloseMainForm.addEventListener("click", function() {
         mainPopUpForm.classList.remove("pop-up_open");
         over.classList.remove("over_open");
+        bodyunLock()
     })
     over.addEventListener("click", function() {
         mainPopUpForm.classList.remove("pop-up_open");
         over.classList.remove("over_open");
+        bodyunLock()
     })
+
+    function bodyLock() {
+
+        const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + 'px';
+
+        if (lockPadding.length > 0) {
+            lockPadding.forEach(el => {
+                el.style.paddingRight = lockPaddingValue;
+            })
+        }
+
+        body.classList.add("body_lock");
+        body.style.paddingRight = lockPaddingValue;
+    }
+    function bodyunLock() {
+        setTimeout(function() {
+
+            if (lockPadding.length > 0) {
+                lockPadding.forEach(el => {
+                    el.style.paddingRight = 0;
+                })
+            }
+
+            body.style.paddingRight = 0;
+            body.classList.remove("body_lock");
+        },timeout);
+    }
 }
 
 export default popUpToggle;
